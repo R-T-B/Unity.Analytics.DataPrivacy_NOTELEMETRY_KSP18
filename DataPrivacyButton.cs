4050b0477d558d7e415fea415be17c74d0184d1d
Raymond Beehler <21GunSoftware@comcast.net>
2019-10-22 14:31:04 -0700

    Yeah, I don't even want those folders to make a mistake, kthx...

--- DataPrivacyButton.cs
+++ DataPrivacyButton.cs
@@ -4,34 +4,34 @@ using UnityEngine.UI;
 
 namespace UnityEngine.Analytics
 {
-  public class DataPrivacyButton : Button
-  {
-    private bool urlOpened;
-
-    private DataPrivacyButton()
+    public class DataPrivacyButton : Button
     {
+        private bool urlOpened;
+
+        private DataPrivacyButton()
+        {
             return;
-    }
+        }
 
-    private void OnFailure(string reason)
-    {
-      return;
-    }
+        private void OnFailure(string reason)
+        {
+            return;
+        }
 
-    private void OpenUrl(string url)
-    {
-      return;
-    }
+        private void OpenUrl(string url)
+        {
+            return;
+        }
 
-    private void OpenDataPrivacyUrl()
-    {
-      this.interactable = false;
-      DataPrivacy.FetchPrivacyUrl(new Action<string>(this.OpenUrl), new Action<string>(this.OnFailure));
-    }
+        private void OpenDataPrivacyUrl()
+        {
+            this.interactable = false;
+            DataPrivacy.FetchPrivacyUrl(new Action<string>(this.OpenUrl), new Action<string>(this.OnFailure));
+        }
 
-    private void OnApplicationFocus(bool hasFocus)
-    {
-      return;
+        private void OnApplicationFocus(bool hasFocus)
+        {
+            return;
+        }
     }
-  }
 }
